3409dbe90aad89eb5de5323d984512295f265d20
John Clem <johnnyclem@gmail.com>
2014-06-03 12:32:49 -0700

    Initial Commit

---
+++ Swift Roster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Roster
+//
+//  Created by John Clem on 6/3/14.
+//  Copyright (c) 2014 Code Fellows. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
