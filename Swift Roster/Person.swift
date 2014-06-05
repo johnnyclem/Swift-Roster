294cfe624b60930040d99cba5e9374a9ed0cf66d
Taylor Potter <taylorkpotter@me.com>
2014-06-04 19:56:29 -0700

    Format part of Person.swift

--- Swift Roster/Person.swift
+++ Swift Roster/Person.swift
@@ -10,64 +10,65 @@ import UIKit
 
 class Person: NSObject {
 
-    var firstName = String()
-    var lastName = String()
-    
-    var github = String()
-    var twitter = String()
-    
-    var favoriteColor = UIColor()
-    var teacher : Bool = false
-    
+    var firstName       = String()
+    var lastName        = String()
+    var github          = String()
+    var twitter         = String()
+    var favoriteColor   = UIColor()
+    var teacher : Bool  = false
     var image : UIImage = UIImage()
-    var hasImage = false
+    var hasImage        = false
     
     func fullName() -> String {
         return firstName + " " + lastName
     }
     
     func initWithName(firstName : String, lastName : String) -> Person {
-        var person = Person()
-        person.firstName = firstName
-        person.lastName = lastName
+        var person           = Person()
+            person.firstName = firstName
+            person.lastName  = lastName
+      
         return person
     }
     
     func tenRandomStudents() -> Person[] {
-        var students = Person[]()
+        var students   = Person[]()
         var firstNames = ["Tony", "Alyssa", "George", "Chris", "Reed", "Joanne", "Sally", "James", "Tom", "Frank"]
-        var lastNames = ["Danza", "Milano", "Foreman", "Meehan", "Sweeney", "Smith", "Jones", "Franco", "Hanks", "Sinatra"]
+        var lastNames  = ["Danza", "Milano", "Foreman", "Meehan", "Sweeney", "Smith", "Jones", "Franco", "Hanks", "Sinatra"]
         
         
         for i in 0..firstNames.count {
-            var person = Person().initWithName(firstNames[i], lastName: lastNames[i])
-            person.github = "@" + person.fullName().lowercaseString
-            person.twitter = person.github
-            person.favoriteColor = randomColor()
-            students += person
+          var person               = Person().initWithName(firstNames[i], lastName: lastNames[i])
+                person.github        = "@" + person.fullName().lowercaseString
+                person.twitter       = person.github
+                person.favoriteColor = randomColor()
+          
+          students += person
         }
         
         return students
     }
     
     func threeRandomTeachers() -> Person[] {
-        var teachers = Person[]()
+        var teachers   = Person[]()
         var firstNames = ["John", "Taylor", "Brad"]
-        var lastNames = ["Clem", "Potter", "Johnson"]
+        var lastNames  = ["Clem", "Potter", "Johnson"]
         
         for i in 0..firstNames.count {
-            var person = Person()
-            person.firstName = firstNames[i]
-            person.lastName = lastNames[i]
-            person.github = "@" + person.fullName().lowercaseString
-            person.twitter = person.github
-            person.favoriteColor = randomColor()
+            var person               = Person()
+                person.firstName     = firstNames[i]
+                person.lastName      = lastNames[i]
+                person.github        = "@" + person.fullName().lowercaseString
+                person.twitter       = person.github
+                person.favoriteColor = randomColor()
+          
             teachers += person
         }
         
         return teachers
     }
-    
+  
+  
     func randomColor() -> UIColor {
         let r = random().bridgeToObjectiveC().floatValue as CGFloat
         let g = random().bridgeToObjectiveC().floatValue as CGFloat
